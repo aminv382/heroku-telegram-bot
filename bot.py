5db7ecd84daabe0c1f3d2e423dfe7603e6f63e2a
aminv382 <58699891+aminv382@users.noreply.github.com>
2021-12-30 19:24:32 +0330

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,20 +1,44 @@
-# -*- coding: utf-8 -*-
-import redis
-import os
+#!/usr/bin/python
+
+# This is a simple echo bot using the decorator mechanism.
+# It echoes any incoming text messages.
+
 import telebot
-# import some_api_lib
-# import ...
+import coin
+API_TOKEN = '5095857991:AAGbQhcB2mvELKlT2LhZ-OnBPXje5QNwTZY'
 
-# Example of your code beginning
-#           Config vars
-token = os.environ['TELEGRAM_TOKEN']
-some_api_token = os.environ['SOME_API_TOKEN']
-#             ...
+bot = telebot.TeleBot(API_TOKEN)
 
-# If you use redis, install this add-on https://elements.heroku.com/addons/heroku-redis
-r = redis.from_url(os.environ.get("REDIS_URL"))
 
-#       Your bot code below
-# bot = telebot.TeleBot(token)
-# some_api = some_api_lib.connect(some_api_token)
-#              ...
+# Handle '/start' and '/help'
+# @bot.message_handler(commands=['help', 'start'])
+# def send_welcome(message):
+#     bot.reply_to(message, """\
+# Hi there, I am EchoBot.
+# I am here to echo your kind words back to you. Just say anything nice and I'll say the exact same thing to you!\
+# """)
+
+
+# Handle all other messages with content_type 'text' (content_types defaults to ['text'])
+# @bot.message_handler(func=lambda message: True)
+# def echo_message(message):
+#     bot.reply_to(message, message.text)
+
+@bot.message_handler(commands=['help'])
+def send_welcome(message):
+    bot.reply_to(message, """\
+Hi there, I am coin bot.
+I am here to give you the coin price as you wish!
+if you want to start enter : /coin \
+""")
+
+@bot.message_handler(commands=['coin'])
+def send_welcome(message):
+    bot.reply_to(message ,"tamam bahar azadi"+coin.get_coin())
+
+@bot.message_handler(func=lambda message: True)
+def echo_message(message):
+    te=coin.get_coin()
+    bot.reply_to(message, coin.text)
+
+bot.infinity_polling()
