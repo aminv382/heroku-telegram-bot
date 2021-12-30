b58db5b2f5d8105b4b32a814cbfb04abdccb8f04
aminv382 <58699891+aminv382@users.noreply.github.com>
2021-12-30 19:25:51 +0330

    Add files via upload

---
+++ coin.py
@@ -0,0 +1,16 @@
+from bs4 import BeautifulSoup
+import pandas 
+import requests
+import re
+url = "https://www.tgju.org/profile/sekeb"
+req = requests.get(url)
+soup = BeautifulSoup(req.text, "html.parser")
+def get_coin():
+    price2=0
+    for link in soup.find_all(class_="value",limit=1):
+    # price=link.get("data-col")
+        price= BeautifulSoup(link.text , "html.parser")
+        price2=price.text.split('\n')[1]
+        break
+    return price2
+    
